dbd7074ca69dcf7c99f34fbaa8b6290b40900f3a
Juancito <cjuan.chona@hotmail.com>
2022-06-15 14:43:08 -0300

    agregando todo a la branch develop

---
+++ Test/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+const deleteFile = (filename,callback)=>{
+    fs.stat(filename,(err,stats)=>{
+        if(err){
+            callback(new Error(`the file ${filename} does not exist`))
+        }else{
+            fs.unlink(filename,(err)=>{
+                if(err){
+                    callback(new Error(`could not delete ${filename}`))
+                } else{
+                    callback()
+                }
+            })
+        }
+    })
+}
+module.exports = deleteFile
