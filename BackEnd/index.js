6bf45e32ea5934e18bbcb589ef07ff700ad6be8b
Juancito <cjuan.chona@hotmail.com>
2022-06-20 10:31:27 -0300

    cookies?

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -15,7 +15,7 @@ const app = express()
 
 // middelwares
 app.use(cors({
-    origin:['http://localhost:4000','http://localhost:3000'],
+    origin:['http://localhost:4000','http://localhost:3000','http://127.0.0.1:3000'],
     credentials:true
 }))
 app.use("/webhook/stripe",express.raw({
